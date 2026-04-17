d39f1106c7d4bdc878f04cce77a5ea43e0108fed
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-04-17 09:21:39 +0530

    module 11 Customize our React Query Properties

--- module11/react-query-template/src/main.jsx
+++ module11/react-query-template/src/main.jsx
@@ -6,7 +6,19 @@ import { BrowserRouter } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      gcTime: 1000 * 60 * 5, // 5 minutes
+      cacheTime: 1000 * 60 * 10, // 10 minutes
+      retry: 3,
+      staleTime: 1000 * 6, // 6 seconds
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    }
+  }
+})
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
