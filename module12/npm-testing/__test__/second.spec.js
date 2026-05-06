f5b2bdf87f8f1f6f0e62ca41ac07789685f3765b
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-05-06 09:54:45 +0530

    module 12 Testing Variables and Functions with Jest: A Simple Start

--- module12/npm-testing/__test__/second.spec.js
+++ module12/npm-testing/__test__/second.spec.js
@@ -1,3 +1,13 @@
+let myVal = 1
+
+function addOne(num) {
+  return num + 1;
+}
+
 test('should add two numbers correctly', () => {
-  expect(1 + 1).toBe(2);
+  let result = myVal + 1;
+  expect(result).toBe(2); 
+  expect(addOne(myVal)).toBe(2);
+  expect(addOne(5)).toBe(6);
+  expect(addOne(3)).toBe(4);
 });
