40c04011f12d47be8fb53a0f29c66f348a76dbd6
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-05-06 09:38:49 +0530

    module 12 Assertions in Jest: Writing Smart and Clear Test cases

--- module12/npm-testing/first.test.js
+++ module12/npm-testing/first.test.js
@@ -1,3 +1,9 @@
-test("my first test", () => {
+describe('will test number cases', () => {
+  test("my first test", () => {
     expect(1 + 1).toBe(2);
 });
+
+it("my second test", () => {
+    expect(2 * 2).toBe(4);
+});
+})
