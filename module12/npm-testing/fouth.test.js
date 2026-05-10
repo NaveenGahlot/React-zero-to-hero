d08c3107d57a2b1cf5f89f9732f98e06dcfb8145
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-05-10 08:32:54 +0530

    module 12 Mastering Matchers in Jest

---
+++ module12/npm-testing/fouth.test.js
@@ -0,0 +1,16 @@
+myVal = 4;
+let name = "John Doe";
+let animals = ["cat", "dog", "rabbit"];
+
+test("Fouth File", ()=>{
+    expect(myVal).not.toBe(1);
+    expect(myVal).toBeGreaterThanOrEqual(4);
+    expect(name).toMatch(/John/);
+    expect(animals).toContain("dog");
+});
+
+test("Object assignment", ()=>{
+    const data = {one: 1};
+    data["two"] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
