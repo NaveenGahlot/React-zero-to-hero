4609b81c5a5c5f21603e4caab6bc51384916719b
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-05-13 08:40:44 +0530

    module 12 Testing Truthy and Falsy Values in Jest

--- module12/npm-testing/fouth.test.js
+++ module12/npm-testing/fouth.test.js
@@ -1,16 +1,23 @@
-myVal = 4;
-let name = "John Doe";
-let animals = ["cat", "dog", "rabbit"];
+// myVal = 4;
+// let name = "John Doe";
+// let animals = ["cat", "dog", "rabbit"];
 
-test("Fouth File", ()=>{
-    expect(myVal).not.toBe(1);
-    expect(myVal).toBeGreaterThanOrEqual(4);
-    expect(name).toMatch(/John/);
-    expect(animals).toContain("dog");
-});
+// test("Fouth File", ()=>{
+//     expect(myVal).not.toBe(1);
+//     expect(myVal).toBeGreaterThanOrEqual(4);
+//     expect(name).toMatch(/John/);
+//     expect(animals).toContain("dog");
+// });
 
-test("Object assignment", ()=>{
-    const data = {one: 1};
-    data["two"] = 2;
-    expect(data).toEqual({one: 1, two: 2});
+// test("Object assignment", ()=>{
+//     const data = {one: 1};
+//     data["two"] = 2;
+//     expect(data).toEqual({one: 1, two: 2});
+// });
+
+let p = "Ajay";
+let a = null;
+test("Should be check truthy or falsy", ()=>{
+    expect(p).toBeTruthy();
+    expect(a).toBeFalsy();
 });
