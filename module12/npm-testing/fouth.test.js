3622694ee8ee8f74639465f8b7918d01a6de6ad2
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-05-14 09:06:13 +0530

    module 12 Handling Errors with Jest: Using toThrow Matcher

--- module12/npm-testing/fouth.test.js
+++ module12/npm-testing/fouth.test.js
@@ -1,23 +1,32 @@
-// myVal = 4;
-// let name = "John Doe";
-// let animals = ["cat", "dog", "rabbit"];
+myVal = 4;
+let name = "John Doe";
+let animals = ["cat", "dog", "rabbit"];
 
-// test("Fouth File", ()=>{
-//     expect(myVal).not.toBe(1);
-//     expect(myVal).toBeGreaterThanOrEqual(4);
-//     expect(name).toMatch(/John/);
-//     expect(animals).toContain("dog");
-// });
+test("Fouth File", ()=>{
+    expect(myVal).not.toBe(1);
+    expect(myVal).toBeGreaterThanOrEqual(4);
+    expect(name).toMatch(/John/);
+    expect(animals).toContain("dog");
+});
 
-// test("Object assignment", ()=>{
-//     const data = {one: 1};
-//     data["two"] = 2;
-//     expect(data).toEqual({one: 1, two: 2});
-// });
+test("Object assignment", ()=>{
+    const data = {one: 1};
+    data["two"] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+});
 
 let p = "Ajay";
 let a = null;
 test("Should be check truthy or falsy", ()=>{
     expect(p).toBeTruthy();
     expect(a).toBeFalsy();
+});
+
+function check(){
+    throw new Error("This is an error");
+}
+test("Should be check error", ()=>{
+    expect(check).toThrow();
+    expect(check).toThrow("This is an error");
+    expect(check).toThrow(/error/);
 });
