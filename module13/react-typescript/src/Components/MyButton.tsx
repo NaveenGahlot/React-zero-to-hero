77f08be420380377a359d862d6de3d0f8a6da1b4
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-06-05 07:37:09 +0530

    module 13 Using Generics in TypeScript State Management

--- module13/react-typescript/src/Components/MyButton.tsx
+++ module13/react-typescript/src/Components/MyButton.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface MyButtonProps {
     title: string | number
     onClick?: () => void
+    isValid?: boolean;
 }
 
-const MyButton: React.FC<MyButtonProps> = ({ title, onClick }) => {
+interface Book {
+    title: string;
+    price: number;
+}
+
+
+const MyButton: React.FC<MyButtonProps> = (props) => {
+  const {title, onClick, isValid} = props;
+  const [value, setValue] = useState<Book>({
+    title: "The Great Gatsby",
+    price: 10.99,
+  })
   return (
     <div>
-      <button onClick={onClick}>{title}</button>
+      <h1>{value.title}</h1>
+      <p>${value.price.toFixed(2)}</p>
+      <button onClick={()=>{setValue({title: "New Title", price: 520})}}>{title}</button>
     </div>
   )
 }
